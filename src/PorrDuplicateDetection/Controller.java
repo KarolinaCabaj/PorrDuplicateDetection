f9a124bfa78f1578922edfeb005c78e3262b34f0
KarolinaCabaj <karolina.anna.cabaj@gmail.com>
2017-11-23 18:51:32 +0100

    zmiana konstruktora Controller

--- src/PorrDuplicateDetection/Controller.java
+++ src/PorrDuplicateDetection/Controller.java
@@ -12,18 +12,20 @@ import java.util.Scanner;
  */
 public class Controller {
 
-    private Text baseText,examinedText;
+    private Text baseText,examinedText, wordsToCheckText;
     private ArrayList<String> wordsListToCheck;
     private ArrayList<Integer> numerOfreats;
 
-    public Controller( Text baseText, Text examinedText, String pathname){
+    public Controller( Text baseText, Text examinedText, Text wordsToCheckText){
         this.baseText=baseText;
         this.examinedText=examinedText;
-        wordsListToCheck= new ArrayList<>();
+        this.wordsToCheckText = wordsToCheckText;
+        wordsListToCheck= wordsToCheckText.getListOfWordsFromText();//new ArrayList<>();
         numerOfreats=new ArrayList<>();
-        loadList(pathname);
+        //loadList(pathname);
 
     }
+    /*
     private void loadList(String pathname)  {
         try {
             Scanner input = new Scanner(new File(pathname));
@@ -38,6 +40,7 @@ public class Controller {
         }
 
     }
+    */
      private HashMap<String,Integer> repeats(ArrayList<String> text){
         HashMap<String,Integer> countMap = new HashMap<>();
         int count;
@@ -59,9 +62,11 @@ public class Controller {
         HashMap<String,Integer> final_list = new HashMap<>();
         for (Map.Entry<String,Integer> entry : a.entrySet())
         {
-            if(  entry.getValue() > b.get(entry.getKey())){
-
+            if(  entry.getValue() < b.get(entry.getKey())){
+                final_list.put(entry.getKey(), entry.getValue());
             }
+            else
+                final_list.put(entry.getKey(), b.get(entry.getKey()));
         }
         return final_list;
     }
