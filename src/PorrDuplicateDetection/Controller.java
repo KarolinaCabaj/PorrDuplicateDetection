9adb9b2be0e96684ed39ed6f3dfeb77a3b94755b
Konradszk <konradszk@gmail.com>
2017-11-23 17:14:43 +0100

    Add some methods to Controller

--- src/PorrDuplicateDetection/Controller.java
+++ src/PorrDuplicateDetection/Controller.java
@@ -1,7 +1,58 @@
 package PorrDuplicateDetection;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by Karolka on 2017-11-23.
  */
 public class Controller {
-}
+
+    private Text baseText,examinedText;
+    private ArrayList<String> wordsListToCheck;
+    private ArrayList<Integer> numerOfreats;
+
+    public Controller( Text baseText, Text examinedText, String pathname){
+        this.baseText=baseText;
+        this.examinedText=examinedText;
+        wordsListToCheck= new ArrayList<>();
+        numerOfreats=new ArrayList<>();
+        loadList(pathname);
+
+    }
+    private void loadList(String pathname)  {
+        try {
+            Scanner input = new Scanner(new File(pathname));
+            while(input.hasNext())
+            {
+                wordsListToCheck.add(input.nextLine());
+            }
+            input.close();
+        }
+        catch(IOException e){
+            System.out.println("Ojoj... coś nie bangla");
+        }
+
+    }
+     private ArrayList<Integer> repeats(ArrayList<String> t){
+        ArrayList<Integer> tmp_list = new ArrayList<>();
+        for (String w:
+             wordsListToCheck) {
+            for (String s:
+                 t) {
+
+
+            }
+                
+            }
+        return tmp_list;
+        }
+
+    }
+
+
+
+
+
